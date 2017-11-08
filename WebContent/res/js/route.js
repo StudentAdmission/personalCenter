436fd125615405007a5b8837f3e0330f17bc43b7
qq283026381 <283026381@qq.com>
2017-11-08 16:47:58 +0800

    -添加班级信息页面路由、宿舍信息页面路由、消息盒子页面路由

--- WebContent/res/js/route.js
+++ WebContent/res/js/route.js
@@ -7,6 +7,21 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
             templateUrl: 'information.html',
             controller: 'informationCtrl'
         })
+        //班级信息页面路由
+        .when('/class', {
+            templateUrl: 'class.html',
+            controller: 'classCtrl'
+        })
+        //宿舍信息页面路由
+        .when('/dormitory', {
+            templateUrl: 'dormitory.html',
+            controller: 'dormitoryCtrl'
+        })
+        //消息盒子页面路由
+        .when('/noticeBox', {
+            templateUrl: 'notice-box.html',
+            controller: 'noticeBoxCtrl'
+        })
         //其他情况路由
         .otherwise({
             redirectTo: '/'
