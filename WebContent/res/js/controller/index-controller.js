24db5fe819efe66e51c1a030a82c1ff8e273b61f
qq283026381 <283026381@qq.com>
2017-11-08 16:46:23 +0800

    -将从数据库获取的个人信息通过index-controller存放在service里，实现在进入个人中心时就获取到后面页面所需的所有个人信息（暂时以变量形式存放）

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -1,45 +1,80 @@
-app.controller('indexCtrl', ['$http', '$scope', 'validateService', function ($http, $scope, validateService) {
-    var permission = '0'; //validateService.validateIdentity();
-    if (permission === -1) {
-        alert('请先登录');
-        window.location.href = '/studentAdmission';
-    } else {
-        if (permission === '0') {
-            $scope.identity = 'student';
-        } else if (permission === '1') {
-            $scope.identity = 'master';
-        } else if (permission === '2') {
-            $scope.identity = 'instructor';
-        }
-        $(function () {
-            function goTop() {
-                $("html,body").animate({scrollTop: 0}, 500);
+app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService', 'personalInfoService',
+    function ($http, $location, $scope, validateService, personalInfoService) {
+        $scope.personInfo = {
+            stdName: "张三",
+            stdNum: '2014010215',
+            stdGender: '女',
+            stdMajor: "软件工程",
+            stdClasssNum: '软工1402',
+            stdDormNum: '1-2-311',
+            stdCollege: '计算机学院',
+            stdTele: '15236255124',
+            stdNation: '汉族',
+            stdAddress: '北京市朝阳区北四环中路35号',
+            stdPostCode: '100101',
+            stdIdentification: '125455466325999845',
+            stdEmail: '154856235@qq.com',
+            stdGrade: '2014',
+            stdNativePlace: '北京',
+            stdFatherName: '李四',
+            stdFatherTele: '145236552365',
+            stdMotherName: '王五',
+            stdMotherTele: '14522251201',
+            stdPoliticalStatus: '团员',
+            stdBirth: '2017-10-02',
+            stdQQ: '125485695',
+            stdWechat: '15248877542',
+            stdAccountMigration: 'true',
+            stdIdPhone: 'certificate-photos.jpg'
+        };
+        personalInfoService.setPersonalInfo($scope.personInfo);
+        var permission = '0'; //validateService.validateIdentity();
+        if (permission === -1) {
+            alert('请先登录');
+            window.location.href = '/studentAdmission';
+        } else {
+            if (permission === '0') {
+                $scope.identity = 'student';
+            } else if (permission === '1') {
+                $scope.identity = 'master';
+            } else if (permission === '2') {
+                $scope.identity = 'instructor';
             }
+            $(function () {
+                function goTop() {
+                    $("html,body").animate({scrollTop: 0}, 500);
+                }
 
-            $('#asideMenu').find('ul').find('li').eq(0).addClass('active');
-            $('#asideMenu ul>li').each(function () {
-                $(this).on('click', function () {
-                    $(this).siblings('li').removeClass('active');
-                    $(this).addClass('active');
+                var currentLocation = $location.$$url;
+                currentLocation = currentLocation.substr(1, currentLocation.length);
+                $('#asideMenu ul>li').each(function () {
+                    if (currentLocation === '') {
+                        $('#asideMenu').find('ul').find('li').eq(0).addClass('active');
+                    } else if ($(this).find('a').attr('href') === currentLocation) {
+                        $(this).addClass('active');
+                    }
+                    $(this).on('click', function () {
+                        $(this).siblings('li').removeClass('active');
+                        $(this).addClass('active');
+                    });
                 });
-            });
-            $('.sa-new-notice').showDetail();
-            var bodyHeight = 0;
-            $(window).on('scroll', function () {
-                if (bodyHeight === 0) {
-                    bodyHeight = $('body').height();
-                    $('#asideMenu').css('height', bodyHeight - 90 + 'px');
-                }
-                var scrollTop = $(this).scrollTop();
-                if (scrollTop > 0) {
-                    $('.go-top').fadeIn();
-                }else{
-                    $('.go-top').fadeOut();
-                }
-            });
-            $('.go-top').on('click',function () {
-                goTop();
-            });
-        })
-    }
-}]);
+                $('.sa-new-notice').showDetail();
+                var bodyHeight = 0;
+                $(window).on('scroll', function () {
+                    bodyHeight = $('.sa-content').height();
+                    if (bodyHeight !== $('#asideMenu').height()) {
+                        $('#asideMenu').height(bodyHeight + 100 + 'px');
+                    }
+                    var scrollTop = $(this).scrollTop();
+                    if (scrollTop > 0) {
+                        $('.go-top').fadeIn();
+                    } else {
+                        $('.go-top').fadeOut();
+                    }
+                });
+                $('.go-top').on('click', function () {
+                    goTop();
+                });
+            })
+        }
+    }]);
