03f91076949d5b5d12e115a9c57d813cf4351379
qq283026381 <283026381@qq.com>
2017-11-09 15:47:55 +0800

    -添加登录信息的获取 -添加对昵称、email、密码的修改btn点击的判断

--- WebContent/res/js/controller/setting-controller.js
+++ WebContent/res/js/controller/setting-controller.js
@@ -1,6 +1,9 @@
-app.controller('settingCtrl', ['$http', '$scope', 'personalInfoService', function ($http, $scope, personalInfoService) {
+app.controller('settingCtrl', ['$http', '$scope', 'validateService', 'personalInfoService', function ($http, $scope, validateService, personalInfoService) {
     $scope.studentInfo = personalInfoService.getPersonalInfo();
-
+    $scope.loginInfo = validateService.getLoginInfo();
+    $scope.nicknameClicked = false;
+    $scope.emailClicked = false;
+    $scope.passwordClicked = false;
     $(function () {
         $('.personal-photo-img').attr('src', 'res/img/avatar.jpg');// + $scope.studentInfo.stdIdPhone);
     })
