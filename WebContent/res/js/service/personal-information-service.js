4437077e107149f1e5c6dd9b2a3e29f1f45babb6
qq283026381 <283026381@qq.com>
2017-11-16 20:20:51 +0800

    -删除不必要的服务

--- WebContent/res/js/service/personal-information-service.js
+++
@@ -1,9 +0,0 @@
-app.service('personalInfoService', [function () {
-    var personalInfo;
-    this.setPersonalInfo = function (person) {
-        personalInfo = person;
-    };
-    this.getPersonalInfo = function () {
-        return personalInfo;
-    };
-}]);
