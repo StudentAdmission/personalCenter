57b55e00c11d7ebfd9aa952857002d22f316700c
qq283026381 <283026381@qq.com>
2017-11-16 20:15:34 +0800

    -从后台获取班级信息

--- WebContent/res/js/controller/class-controller.js
+++ WebContent/res/js/controller/class-controller.js
@@ -1,3 +1,40 @@
-app.controller('classCtrl', ['$http', '$scope','personalInfoService', function ($http, $scope,personalInfoService) {
-    $scope.personalInfo=personalInfoService.getPersonalInfo();
+app.controller('classCtrl', ['$http', '$scope', 'validateService', function ($http, $scope, validateService) {
+    if (!$scope.personalInfo) {
+        $http.post('/studentAdmission/getPersonalInfo.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+            var content = '';
+            if (response.status !== 200) {
+                content += NETWORK_ERROR;
+            } else {
+                var personData = response.data;
+                if (personData.status !== 1) {
+                    content += SERVER_ERROR;
+                } else {
+                    $scope.personalInfo = personData.data;
+                }
+            }
+            if (content !== '') {
+                $('.class-main').empty().append(content);
+            }
+        });
+    }
+    $http.post('/studentAdmission/getClass.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+        var content = '';
+        if (response.status !== 200) {
+            content += NETWORK_ERROR;
+        } else {
+            var classData = response.data;
+            if (classData.status !== 1) {
+                content += SERVER_ERROR;
+            } else {
+                console.log(classData);
+                $scope.classmate = classData.data.classmate;
+                $scope.classmaster = classData.data.classmaster;
+                $scope.instructor = classData.data.instructor;
+            }
+        }
+        if (content !== '') {
+            $('.class-main').empty().append(content);
+        }
+    })
+
 }]);
