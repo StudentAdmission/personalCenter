5859a36459b1eec20c51a3dd94c78f18cd73a9ee
qq283026381 <283026381@qq.com>
2017-11-16 11:17:58 +0800

    -个人信息通过后台获取

--- WebContent/res/js/controller/information-controller.js
+++ WebContent/res/js/controller/information-controller.js
@@ -1,6 +1,26 @@
-app.controller('informationCtrl', ['$http', '$scope', 'personalInfoService', function ($http, $scope, personalInfoService) {
-    $scope.studentInfo=personalInfoService.getPersonalInfo();
+app.controller('informationCtrl', ['$http', '$scope', 'validateService', 'personalInfoService', function ($http, $scope, validateService, personalInfoService) {
+    if (!$scope.studentInfo) {
+        $http.post('/studentAdmission/getPersonalInfo.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+            var content = '';
+            if (response.status !== 200) {
+                content += '<div class="connect-error">网络异常，请检查网络连接</div>';
+            } else {
+                var personData = response.data;
+                if (personData.status !== 1) {
+                    content += '<div class="connect-error">数据库连接失败，请联系管理员</div>';
+                } else {
+                    personalInfoService.setPersonalInfo(personData.data);
+                    $scope.studentInfo = personData.data;
+                    $('.certificate-photo-img').attr('src', 'res/img/' + $scope.studentInfo.stdIdPhoto);
+                }
+            }
+            if (content !== '') {
+                $('.personal-information .information-detail').empty().append(content);
+            }
+        });
+    }
+
     $(function () {
-        $('.certificate-photo-img').attr('src', 'res/img/' + $scope.studentInfo.stdIdPhone);
+
     });
 }]);
