f0b426b9377c5dc2349ad62b4574f0cb8966dbd6
qq283026381 <283026381@qq.com>
2017-11-16 20:16:25 +0800

    -更改session存放内容，只留下登录名

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -1,5 +1,5 @@
-app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService', 'personalInfoService',
-    function ($http, $location, $scope, validateService, personalInfoService) {
+app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService',
+    function ($http, $location, $scope, validateService) {
         $scope.logout = function () {
             validateService.logout();
             toastr.success('注销成功，2s后返回主页');
@@ -7,62 +7,71 @@ app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService', 
                 window.location.href = '/studentAdmission';
             }, 2000);
         };
-        var permission = validateService.validateIdentity();
-        if (permission === -1) {
-            alert('请先登录');
-            window.location.href = '/studentAdmission';
-        } else {
-            if (permission === '0') {
-                $scope.identity = 'student';
-            } else if (permission === '1') {
-                $scope.identity = 'master';
-            } else if (permission === '2') {
-                $scope.identity = 'instructor';
-            }
-
-            $scope.loginInfo = {
-                loginNickname: validateService.getLoginSessionNickname(),
-                loginNum: validateService.getLoginSession(),
-                loginPortrait: validateService.getLoginSessionPortrait(),
-                loginTag: validateService.getLoginSessionTag()
-            };
-            validateService.setLoginInfo($scope.loginInfo);
-
-            $('.sa-avatar').attr('src', 'res/img/' + $scope.loginInfo.loginPortrait);
-
-            $(function () {
-                function goTop() {
-                    $("html,body").animate({scrollTop: 0}, 500);
+        if (validateService.getLoginSession()) {
+            //登录后验证模块
+            setInterval(function () {
+                var session = validateService.getLoginSession() ? validateService.getLoginSession() : '';
+                if (session) {
+                    validateService.validateIdentity(session);
+                }
+            }, 30000);
+            $http.post('/studentAdmission/getLogin.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+                $scope.loginInfo = {
+                    loginNickname: response.data.data.loginNickname,
+                    loginNum: response.data.data.loginNum,
+                    loginPortrait: response.data.data.loginPortrait,
+                    loginTag: response.data.data.loginTag,
+                    loginEmail: response.data.data.loginEmail
+                }
+                $('.sa-avatar').attr('src', response.data.data.loginPortrait ? (AVATAR_PATH + response.data.data.loginPortrait) : 'res/img/avatar.jpg');
+                if ($scope.loginInfo.loginTag === 0) {
+                    $scope.identity = 'student';
+                } else if ($scope.loginInfo.loginTag === 1) {
+                    $scope.identity = 'master';
+                } else if ($scope.loginInfo.loginTag === 2) {
+                    $scope.identity = 'instructor';
+                } else {
+                    alert('请先登录');
+                    window.location.href = '/studentAdmission';
                 }
 
-                var currentLocation = $location.$$url;
-                currentLocation = currentLocation.substr(1, currentLocation.length);
-                $('#asideMenu ul>li').each(function () {
-                    if ($(this).find('a').attr('href') === currentLocation) {
-                        $(this).addClass('active');
+                $(function () {
+                    function goTop() {
+                        $("html,body").animate({scrollTop: 0}, 500);
                     }
-                    $(this).on('click', function () {
-                        $(this).siblings('li').removeClass('active');
-                        $(this).addClass('active');
+
+                    var currentLocation = $location.$$url;
+                    currentLocation = currentLocation.substr(1, currentLocation.length);
+                    $('#asideMenu ul>li').each(function () {
+                        if ($(this).find('a').attr('href') === currentLocation) {
+                            $(this).addClass('active');
+                        }
+                        $(this).on('click', function () {
+                            $(this).siblings('li').removeClass('active');
+                            $(this).addClass('active');
+                        });
                     });
-                });
-                $('.sa-new-notice').showDetail();
-                $(window).on('scroll', function () {
-                    var bodyHeight = $('.sa-content').height();
-                    var windowHeight = $(window).height();
-                    if (bodyHeight !== $('#asideMenu').height()) {
-                        $('#asideMenu').height((bodyHeight > windowHeight ? bodyHeight : windowHeight) + 100 + 'px');
-                    }
-                    var scrollTop = $(this).scrollTop();
-                    if (scrollTop > 0) {
-                        $('.go-top').fadeIn();
-                    } else {
-                        $('.go-top').fadeOut();
-                    }
-                });
-                $('.go-top').on('click', function () {
-                    goTop();
-                });
-            })
+                    $('.sa-new-notice').showDetail();
+                    $(window).on('scroll', function () {
+                        var bodyHeight = $('.sa-content').height();
+                        var windowHeight = $(window).height();
+                        if (bodyHeight !== $('#asideMenu').height()) {
+                            $('#asideMenu').height((bodyHeight > windowHeight ? bodyHeight : windowHeight) + 100 + 'px');
+                        }
+                        var scrollTop = $(this).scrollTop();
+                        if (scrollTop > 0) {
+                            $('.go-top').fadeIn();
+                        } else {
+                            $('.go-top').fadeOut();
+                        }
+                    });
+                    $('.go-top').on('click', function () {
+                        goTop();
+                    });
+                })
+            });
+        } else {
+            alert('请先登录');
+            window.location.href = '/studentAdmission';
         }
     }]);
