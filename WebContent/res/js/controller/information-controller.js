6178f07377f26aa618bd52b06f3fb7d562028348
qq283026381 <283026381@qq.com>
2017-11-16 20:17:09 +0800

    -调整结构

--- WebContent/res/js/controller/information-controller.js
+++ WebContent/res/js/controller/information-controller.js
@@ -1,26 +1,20 @@
-app.controller('informationCtrl', ['$http', '$scope', 'validateService', 'personalInfoService', function ($http, $scope, validateService, personalInfoService) {
-    if (!$scope.studentInfo) {
-        $http.post('/studentAdmission/getPersonalInfo.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
-            var content = '';
-            if (response.status !== 200) {
-                content += '<div class="connect-error">网络异常，请检查网络连接</div>';
+app.controller('informationCtrl', ['$http', '$scope', 'validateService', function ($http, $scope, validateService) {
+    $http.post('/studentAdmission/getPersonalInfo.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+        var content = '';
+        if (response.status !== 200) {
+            content += NETWORK_ERROR;
+        } else {
+            var personData = response.data;
+            if (personData.status !== 1) {
+                content += SERVER_ERROR;
             } else {
-                var personData = response.data;
-                if (personData.status !== 1) {
-                    content += '<div class="connect-error">数据库连接失败，请联系管理员</div>';
-                } else {
-                    personalInfoService.setPersonalInfo(personData.data);
-                    $scope.studentInfo = personData.data;
-                    $('.certificate-photo-img').attr('src', 'res/img/' + $scope.studentInfo.stdIdPhoto);
-                }
-            }
-            if (content !== '') {
-                $('.personal-information .information-detail').empty().append(content);
-            }
-        });
-    }
+                $scope.studentInfo = personData.data;
+                $('.certificate-photo-img').attr('src', $scope.studentInfo.stdIdPhoto ? (ID_PHOTO_PATH + $scope.studentInfo.stdIdPhoto) : 'res/img/certificate-photos.jpg');
 
-    $(function () {
-
+            }
+        }
+        if (content !== '') {
+            $('.personal-information .information-detail').empty().append(content);
+        }
     });
 }]);
