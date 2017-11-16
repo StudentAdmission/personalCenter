b6dc08fd6fd06cd32af92c383b6cb381b7baa442
qq283026381 <283026381@qq.com>
2017-11-16 20:15:45 +0800

    -从后台获取宿舍信息

--- WebContent/res/js/controller/dormitory-controller.js
+++ WebContent/res/js/controller/dormitory-controller.js
@@ -1,3 +1,24 @@
-app.controller('dormitoryCtrl', ['$http', '$scope', 'personalInfoService', function ($http, $scope, personalInfoService) {
-    $scope.personInfo = personalInfoService.getPersonalInfo();
+app.controller('dormitoryCtrl', ['$http', '$scope', 'validateService', function ($http, $scope, validateService) {
+    if (!$scope.personInfo) {
+        $http.post('/studentAdmission/getPersonalInfo.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+            var content = '';
+            if (response.status !== 200) {
+                content += NETWORK_ERROR;
+            } else {
+                var personData = response.data;
+                if (personData.status !== 1) {
+                    content += SERVER_ERROR;
+                } else {
+                    $scope.personInfo = personData.data;
+                }
+            }
+            if (content !== '') {
+                $('.dormitory-main').empty().append(content);
+            }
+        });
+    }
+    $http.post('/studentAdmission/getDorm.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+        $scope.dormmate=response.data.data;
+    })
+
 }]);
