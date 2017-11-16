15a95ab7ffba41a9d36997f4a241d2e254d2ee98
qq283026381 <283026381@qq.com>
2017-11-16 11:16:37 +0800

    -设置个人信息从有效性服务中获取

--- WebContent/res/js/controller/index-controller.js
+++ WebContent/res/js/controller/index-controller.js
@@ -1,38 +1,5 @@
 app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService', 'personalInfoService',
     function ($http, $location, $scope, validateService, personalInfoService) {
-        $scope.personInfo = {
-            stdName: "张三",
-            stdNum: '2014010215',
-            stdGender: '女',
-            stdMajor: "软件工程",
-            stdClasssNum: '软工1402',
-            stdDormNum: '1-2-311',
-            stdCollege: '计算机学院',
-            stdTele: '15236255124',
-            stdNation: '汉族',
-            stdAddress: '北京市朝阳区北四环中路35号',
-            stdPostCode: '100101',
-            stdIdentification: '125455466325999845',
-            stdEmail: '154856235@qq.com',
-            stdGrade: '2014',
-            stdNativePlace: '北京',
-            stdFatherName: '李四',
-            stdFatherTele: '145236552365',
-            stdMotherName: '王五',
-            stdMotherTele: '14522251201',
-            stdPoliticalStatus: '团员',
-            stdBirth: '2017-10-02',
-            stdQQ: '125485695',
-            stdWechat: '15248877542',
-            stdAccountMigration: 'true',
-            stdIdPhone: 'certificate-photos.jpg'
-        };
-        $scope.loginInfo = {
-            loginNickname: '张三',
-            loginEmail: '123456@163.com'
-        };
-        personalInfoService.setPersonalInfo($scope.personInfo);
-        validateService.setLoginInfo($scope.loginInfo);
         $scope.logout = function () {
             validateService.logout();
             toastr.success('注销成功，2s后返回主页');
@@ -52,6 +19,17 @@ app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService', 
             } else if (permission === '2') {
                 $scope.identity = 'instructor';
             }
+
+            $scope.loginInfo = {
+                loginNickname: validateService.getLoginSessionNickname(),
+                loginNum: validateService.getLoginSession(),
+                loginPortrait: validateService.getLoginSessionPortrait(),
+                loginTag: validateService.getLoginSessionTag()
+            };
+            validateService.setLoginInfo($scope.loginInfo);
+
+            $('.sa-avatar').attr('src', 'res/img/' + $scope.loginInfo.loginPortrait);
+
             $(function () {
                 function goTop() {
                     $("html,body").animate({scrollTop: 0}, 500);
@@ -60,9 +38,7 @@ app.controller('indexCtrl', ['$http', '$location', '$scope', 'validateService', 
                 var currentLocation = $location.$$url;
                 currentLocation = currentLocation.substr(1, currentLocation.length);
                 $('#asideMenu ul>li').each(function () {
-                    if (currentLocation === '') {
-                        $('#asideMenu').find('ul').find('li').eq(0).addClass('active');
-                    } else if ($(this).find('a').attr('href') === currentLocation) {
+                    if ($(this).find('a').attr('href') === currentLocation) {
                         $(this).addClass('active');
                     }
                     $(this).on('click', function () {
