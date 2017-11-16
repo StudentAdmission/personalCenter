19eb913a650bf6b45b6dc4709783fdb1f0d6c0d9
qq283026381 <283026381@qq.com>
2017-11-16 20:17:59 +0800

    -删除多余存放信息

--- WebContent/res/js/service/validate-service.js
+++ WebContent/res/js/service/validate-service.js
@@ -1,49 +1,21 @@
 app.service('validateService', ['$http', function ($http) {
-    var loginInfo;
     this.setLoginSession = function (session) {
         localStorage.saLogin = session;
-    };
-    this.setLoginSessionTag = function (tag) {
-        localStorage.saTag = tag;
-    };
-    this.setLoginInfo = function (login) {
-        loginInfo = login;
-    };
-    this.getLoginInfo = function () {
-        return loginInfo;
-    };
-    this.getLoginSessionTag = function () {
-        return localStorage.saTag;
     };
     this.getLoginSession = function () {
         return localStorage.saLogin;
     };
-    this.getLoginSessionNickname = function () {
-        return localStorage.saNickname;
-    };
-    this.getLoginSessionPortrait = function () {
-        return localStorage.saPortrait;
+    this.logout = function () {
+        this.setLoginSession('');
     };
 
-    this.validateIdentity = function () {
-        var num = this.getLoginSession();
-        var permission = this.getLoginSessionTag();
-        if (num === '' || permission === '') {
-            alert('请先登录');
-            window.location.href = '/studentAdmission';
-        }
+    this.validateIdentity = function (num) {
         $http.post('/studentAdmission/login/gettime.do', {loginNum: num}).then(function (response) {
             var currentTime = new Date().getTime();
             if (currentTime >= response.data.data.loginTime) {
                 alert('登录已失效，请重新登录');
                 window.location.href = '/studentAdmission';
-                permission = -1;
             }
         });
-        return permission;
-    };
-    this.logout = function () {
-        this.setLoginSession('');
-        this.setLoginSessionTag('');
     }
 }]);
