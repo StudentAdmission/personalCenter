eee7025bd16e9d49d97598104f6297b1a9aeb60b
qq283026381 <283026381@qq.com>
2017-11-17 17:02:02 +0800

    -从后台获取本人消息通知 -点击未读消息后，设置消息盒子标记为已读

--- WebContent/res/js/controller/notice-box-controller.js
+++ WebContent/res/js/controller/notice-box-controller.js
@@ -1,10 +1,38 @@
-app.controller('noticeBoxCtrl', ['$http', '$scope', 'personalInfoService', function ($http, $scope, personalInfoService) {
+app.controller('noticeBoxCtrl', ['$http', '$scope', 'validateService', function ($http, $scope, validateService) {
 
-    $(function () {
-        $('.notice-box-item').on('click', function () {
-            $(this).removeClass('unread');
-            $(this).find('.notice-condition').toggleClass('opened');
-            $(this).find('.notice-detail').slideToggle();
-        })
-    })
+    $http.post('/studentAdmission/getAllNotice.do', "'" + validateService.getLoginSession() + "'").then(function (response) {
+        var content = '';
+        if (response.status !== 200) {
+            content += NETWORK_ERROR;
+        } else {
+            var noticeBoxData = response.data;
+            if (noticeBoxData.status !== 1) {
+                content += SERVER_ERROR;
+            } else {
+                $scope.noticeBox = noticeBoxData.data;
+            }
+        }
+        if (content !== '') {
+            $('.notice-box-main').empty().append(content);
+        }
+        $scope.toggleNotice = function (id) {
+            var self = '.' + id;
+            $(self).removeClass('unread');
+            $(self).find('.notice-condition').toggleClass('opened');
+            $(self).find('.notice-detail').slideToggle();
+        }
+    });
+    $scope.setReadTag = function (id) {
+        $scope.readTag = {
+            smbStudentNum: validateService.getLoginSession(),
+            smbNoticeId: id
+        }
+        $http.post('/studentAdmission/updateReadTag.do',$scope.readTag).then(function (response) {
+            console.log(response);
+        });
+        $http.post('/studentAdmission/hasUnread.do', "'" + validateService.getLoginSession() + "'")
+            .then(function (response) {
+                $scope.hasUnreadNotice = response.data.status === 1;
+            });
+    }
 }]);
